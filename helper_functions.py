6505c7f0568813919d75f48c9766d57566f4ffd0
Ben Levine <60677145+bclevine@users.noreply.github.com>
2023-08-31 16:37:57 -0400

    Updated with final plot notebooks

--- helper_functions.py
+++ helper_functions.py
@@ -89,12 +89,16 @@ def load_pkl(filename):
 
 # PREPROCESS DATA VECTOR
 def preprocess(data_vector, angles):
-    data, nz_data, cov = data_vector
-    masks = [np.where(data[f"theta_{i}"] > angles[i])[0] for i in range(len(angles))]
+    masks = [
+        np.where(data_vector[0][f"theta_{i}"] > angles[i])[0]
+        for i in range(len(angles))
+    ]
     inv_cov = np.array(
         [
             np.linalg.inv(
-                cov[i][masks[i][0] : masks[i][-1] + 1, masks[i][0] : masks[i][-1] + 1]
+                data_vector[2][i][
+                    masks[i][0] : masks[i][-1] + 1, masks[i][0] : masks[i][-1] + 1
+                ]
             )
             for i in range(5)
         ]
@@ -103,20 +107,42 @@ def preprocess(data_vector, angles):
 
 
 # HELPER FUNCTION TO CLEAN UP CODE IN MODEL FUNCTION
-def model_ccl_helper(data, b, nz_data, cosmo):
+def model_ccl_helper(data, b, nz_data, cosmo, mag_bias=None, has_rsd=False):
+    mb = (
+        [None for i in range(5)]
+        if mag_bias is None
+        else [
+            (nz_data[f"z_{i}"], mag_bias[i] * np.ones_like(nz_data[f"z_{i}"]))
+            for i in range(5)
+        ]
+    )
     tracers = [
         ccl.NumberCountsTracer(
             cosmo,
-            True,
+            has_rsd=has_rsd,
             dndz=(nz_data[f"z_{i}"], nz_data[f"Nz_{i}"]),
             bias=(nz_data[f"z_{i}"], b[i] * np.ones_like(nz_data[f"z_{i}"])),
+            mag_bias=mb[i],
         )
         for i in range(5)
     ]
-    ell = np.unique(np.geomspace(0.1, 30000, 75).astype(int))
+    ell = np.unique(np.geomspace(1, 30000, 250).astype(np.int32))
     cls_all = [ccl.angular_cl(cosmo, tracers[i], tracers[i], ell) for i in range(5)]
     return [
         ccl.correlation(cosmo, ell, cls_all[i], data[f"theta_{i}"] / 60)
+        for i in range(5)
+    ]
+
+
+# CHI2
+def chi2(data, w_model, inv_cov, masks):
+    return [
+        np.einsum(
+            "i, ij, j",
+            data[f"w_{i}"][masks[i]] - w_model[i][masks[i]],
+            inv_cov[i],
+            data[f"w_{i}"][masks[i]] - w_model[i][masks[i]],
+        )
         for i in range(5)
     ]
 
@@ -127,18 +153,10 @@ def log_like(pars, data, nz_data, inv_cov, masks, model, prior):
     if np.isfinite(pri):
         try:
             w_model = model(pars, data, nz_data)
-            resid = [
-                np.einsum(
-                    "i, ij, j",
-                    data[f"w_{i}"][masks[i]] - w_model[i][masks[i]],
-                    inv_cov[i],
-                    data[f"w_{i}"][masks[i]] - w_model[i][masks[i]],
-                )
-                for i in range(5)
-            ]
-            resid = -0.5 * np.sum(resid)
+            resid = -0.5 * np.sum(chi2(data, w_model, inv_cov, masks))
             return resid + pri
-        except Exception:
+        except Exception as e:
+            print(e)
             return -np.inf
     else:
         return pri
@@ -149,11 +167,19 @@ def min_func(*args):
     return -1 * log_like(*args)
 
 
+# FUNCTION TO RUN THE EMCEE CHAIN
 def run_emcee(
-    data_vector, filename, initial, angle_list, model, prior, nsteps=700, scatter=0.05
+    data_vector,
+    filename,
+    initial,
+    angle_list,
+    model,
+    prior,
+    nsteps=700,
+    scatter=0.05,
+    posteriors=False,
 ):
     # SET UP VARIABLES AND DATA
-    data, nz_data, cov = data_vector
     masks, inv_cov = preprocess(data_vector, angle_list)
     pos = initial + scatter * np.random.randn(32, len(initial))
     nwalkers, ndim = pos.shape
@@ -166,12 +192,15 @@ def run_emcee(
         nwalkers,
         ndim,
         log_like,
-        args=(data, nz_data, inv_cov, masks, model, prior),
+        args=(data_vector[0], data_vector[1], inv_cov, masks, model, prior),
         backend=backend,
     )
     sampler.run_mcmc(pos, nsteps, progress=True)
     flat_samples = sampler.get_chain(flat=True)
-    np.save(filename, flat_samples)
+    if posteriors:
+        np.save(filename, [flat_samples, sampler.get_log_prob(flat=True)])
+    else:
+        np.save(filename, flat_samples)
 
 
 # PLOT WALKER CONVERGENCE
@@ -239,7 +268,6 @@ def process_stuck_chain(chain, trim_length):
 # DATA PROCESSOR CLASS
 class Data_Processor:
     def __init__(self, data_vector, chain, model, parnames, fit_vals=None):
-        data, nz_data, cov = data_vector
         # LOAD AND ANALYZE POSTERIOR
         if (chain is not None) and (fit_vals is None):
             c = ChainConsumer()
@@ -251,12 +279,12 @@ class Data_Processor:
             best_fits = [post_dict[key][1] for key in post_dict.keys()]
             self.best_fits = np.array(best_fits)
         else:
-            print("Fitting custom parameters.")
+            # print("Fitting custom parameters.")
             self.best_fits = np.array(fit_vals)
-        print("Best Fits:", self.best_fits)
+        # print("Best Fits:", self.best_fits)
 
         # GENERATE MODEL CORRELATION
-        self.ccl_guess = model(self.best_fits, data, nz_data)
+        self.ccl_guess = model(self.best_fits, data_vector[0], data_vector[1])
 
 
 # MODEL PLOTTER
@@ -269,16 +297,9 @@ def plot_model(
     # Calculate Chi2
     masks, inv_cov = preprocess(data_vector, angle_list)
     w_model = processor.ccl_guess
-    resid = [
-        np.einsum(
-            "i, ij, j",
-            data[f"w_{i}"][masks[i]] - w_model[i][masks[i]],
-            inv_cov[i],
-            data[f"w_{i}"][masks[i]] - w_model[i][masks[i]],
-        )
-        for i in range(5)
-    ]
+    resid = chi2(data, w_model, inv_cov, masks)
     n_dof = [len(data[f"w_{i}"][masks[i]]) for i in range(len(masks))]
+    n_pars = len(parnames)
 
     # PLOT
     fig, axs = plt.subplots(1, 5, sharey=True, figsize=(15, 3))
@@ -307,7 +328,7 @@ def plot_model(
         axs[i].text(
             0.95,
             0.9,
-            f"$\\chi^2/\\nu$ = {resid[i]:.1f}/{n_dof[i]:.0f}",
+            f"$\\chi^2/\\nu$ = {resid[i]:.1f}/({n_dof[i]:.0f}",
             horizontalalignment="right",
             verticalalignment="center",
             transform=axs[i].transAxes,
@@ -315,7 +336,7 @@ def plot_model(
         if i == 0:
             axs[i].set_ylabel("$w(\\theta)$")
     fig.suptitle(
-        f"{label}; pars = {np.round(processor.best_fits, 3)}; $\\chi^2/\\nu$ = {np.sum(resid):.1f}/{np.sum(n_dof)}",
+        f"{label}; pars = {np.round(processor.best_fits, 3)}; $\\chi^2/\\nu$ = {np.sum(resid):.1f}/{np.sum(n_dof)}-{n_pars}",
         y=1.1,
     )
     plt.show()
@@ -323,50 +344,72 @@ def plot_model(
 
 # MODEL+RESIDUAL PLOTTER
 def plot_model_residual(
-    data_vector, chain, model, label, angle_list, parnames, fit_vals=None, color="C1"
+    data_vector,
+    chain,
+    model,
+    label,
+    angle_list,
+    parnames,
+    fit_vals=None,
+    color="C1",
+    use_cov=True,
+    resid_limits=(-3, 2),
+    delta_or_ratio="delta",
+    yticks=None,
 ):
+    # STYLES
+    marker = "."
+    capsize = 2
+    ls = "-"
+    markersize = 7
+    lw = 1.5
+    model_color = "red"
+
+    # CODE BEGINS
     data, nz, cov = data_vector
     processor = Data_Processor(data_vector, chain, model, parnames, fit_vals)
 
     # Calculate Chi2
     masks, inv_cov = preprocess(data_vector, angle_list)
     w_model = processor.ccl_guess
-    resid = [
-        np.einsum(
-            "i, ij, j",
-            data[f"w_{i}"][masks[i]] - w_model[i][masks[i]],
-            inv_cov[i],
-            data[f"w_{i}"][masks[i]] - w_model[i][masks[i]],
-        )
-        for i in range(5)
-    ]
+    resid = chi2(data, w_model, inv_cov, masks)
     n_dof = [len(data[f"w_{i}"][masks[i]]) for i in range(len(masks))]
+    n_pars = len(parnames)
+    resid_label = (
+        "$\\Delta w/\\sigma_{w}$" if delta_or_ratio == "delta" else "$w_{obs}/w_{CCL}$"
+    )
 
     # PLOT
     fig, axs = plt.subplots(2, 5, figsize=(15, 3), height_ratios=[4, 1])
     for i in range(len(axs[1])):
+        err = np.sqrt(cov[i].diagonal()) if use_cov else data[f"werr_{i}"]
         axs[0][i].set_title(
             f"${np.round((.2*i)+.2, 2)} < z < {np.round((.2*i)+.4, 2)}$"
         )
-        axs[0][i].axvspan(0, angle_list[i], alpha=0.2)
+        axs[0][i].axvspan(
+            0, angle_list[i], alpha=0.1, edgecolor="black", facecolor="gray"
+        )
         axs[0][i].errorbar(
             data[f"theta_{i}"],
             data[f"w_{i}"],
-            np.sqrt(cov[i].diagonal()),
-            # data[f"werr_{i}"],
+            err,
             label=label,
             color=color,
+            marker=marker,
+            capsize=capsize,
+            ls=ls,
+            markersize=markersize,
+            lw=lw,
         )
         axs[0][i].plot(
             data[f"theta_{i}"][len(data[f"theta_{i}"]) - len(processor.ccl_guess[i]) :],
             processor.ccl_guess[i],
             label="Best Fit Model",
-            color="red",
+            color=model_color,
+            lw=lw,
         )
         residual_plot_helper(axs, 0, i, "log")
         axs[0][i].set_ylim(4e-5, 0.1)
-        axs[0][i].legend(loc="lower left")
-        # axs[0][i].set_xlabel("$\\theta$ [arcmin]")
         axs[0][i].text(
             0.95,
             0.9,
@@ -377,35 +420,57 @@ def plot_model_residual(
         )
         axs[0][i].axes.xaxis.set_ticklabels([])
         # RESIDUALS
-        axs[1][i].axvspan(0, angle_list[i], alpha=0.2)
-        axs[1][i].axhline(0, 0, 500, color="black", ls=":")
-        axs[1][i].plot(
-            data[f"theta_{i}"],
-            (data[f"w_{i}"] - processor.ccl_guess[i])
-            / np.sqrt(cov[i].diagonal()),  # data[f"werr_{i}"],
-            color=color,
+        axs[1][i].axvspan(
+            0, angle_list[i], alpha=0.1, edgecolor="black", facecolor="gray"
         )
+        if delta_or_ratio == "ratio":
+            axs[1][i].axhline(1, 0, 500, color="black", ls=":")
+            axs[1][i].errorbar(
+                data[f"theta_{i}"],
+                (data[f"w_{i}"] / processor.ccl_guess[i]),
+                (err / np.abs(processor.ccl_guess[i])),
+                color=color,
+                marker=marker,
+                capsize=capsize,
+                ls=ls,
+                markersize=markersize,
+                lw=lw,
+            )
+        else:
+            axs[1][i].axhline(0, 0, 500, color="black", ls=":")
+            axs[1][i].plot(
+                data[f"theta_{i}"],
+                (data[f"w_{i}"] - processor.ccl_guess[i]) / err,
+                color=color,
+                marker=marker,
+                ls=ls,
+                markersize=markersize,
+                lw=lw,
+            )
         residual_plot_helper(axs, 1, i, "linear")
-        axs[1][i].set_ylim(-3, 2)
+        axs[1][i].set_ylim(resid_limits[0], resid_limits[1])
         axs[1][i].set_xlabel("$\\theta$ [arcmin]")
+        if yticks is not None:
+            axs[1][i].set_yticks(yticks)
         if i == 0:
+            axs[0][i].legend(loc="lower left")
             axs[0][i].set_ylabel("$w(\\theta)$")
-            axs[1][i].set_ylabel("$(w_{data} - w_{model})/\\sigma_{w}$")
-        if i != 0:
+            axs[1][i].set_ylabel(resid_label)
+        else:
             axs[0][i].axes.yaxis.set_ticklabels([])
             axs[1][i].axes.yaxis.set_ticklabels([])
     fig.suptitle(
-        f"{label}; pars = {np.round(processor.best_fits, 3)}; $\\chi^2/\\nu$ = {np.sum(resid):.1f}/{np.sum(n_dof)}",
+        f"{label}; pars = {np.round(processor.best_fits, 3)}; $\\chi^2/\\nu$ = {np.sum(resid):.1f}/({np.sum(n_dof)}-{n_pars})",
         y=1.1,
     )
-    fig.subplots_adjust(hspace=0)
+    fig.subplots_adjust(hspace=0, wspace=0.1)
     plt.show()
 
 
 def residual_plot_helper(axs, idx, i, yscale):
     axs[idx][i].set_xscale("log")
     axs[idx][i].set_yscale(yscale)
-    axs[idx][i].set_xlim(2, 250)
+    axs[idx][i].set_xlim(1, 250)
 
 
 # GRADIENT MINIMIZATION FUNCTION
